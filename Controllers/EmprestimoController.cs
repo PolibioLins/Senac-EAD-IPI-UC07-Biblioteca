4df206322666b7cd6376824e63303dff3025ebed
PolibioLins <polibio.l.costa@gmail.com>
2021-10-10 11:28:23 -0300

    Atualizado o dropdown de livros em cadastro e dicao de emprestimo para apenas livros disponiveis

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -16,7 +16,7 @@ namespace Biblioteca.Controllers
             EmprestimoService emprestimoService = new EmprestimoService();
 
             CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarTodos();
+            cadModel.Livros = livroService.ListarDisponiveis();
             return View(cadModel);
         }
 
@@ -59,7 +59,7 @@ namespace Biblioteca.Controllers
             Emprestimo e = em.ObterPorId(id);
 
             CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarTodos();
+            cadModel.Livros = livroService.ListarDisponiveis();
             cadModel.Emprestimo = e;
             
             return View(cadModel);
